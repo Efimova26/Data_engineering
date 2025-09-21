d8f52b103ad19f790a5cf6e82c899aedc2bc4d34
Efimova26 <efimovarja@gmail.com>
2025-09-21 19:16:22 +0300

    Add files

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,9 @@
-import pandas as pd
 
-FILE_ID = "1BlN2qCu0WEVX6rHUBQw56wsljv7kqaJQ"
-file_url = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
+import pandas as pd
+import os
+
+FILE_ID = os.getenv("FILE_ID")
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
 raw_data = pd.read_csv(file_url)
-raw_data.head(10) 
+print(raw_data.head(10))
