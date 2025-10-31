ad7d27c16355e36c0c3b29169d5d09158fd689af
Efimova26 <efimovarja@gmail.com>
2025-10-31 13:55:03 +0300

    Обновлен main.py: добавлена валидация и загрузка в базу

--- etl/main.py
+++ etl/main.py
@@ -1,23 +1,37 @@
 import argparse
 from etl.extract import extract_data, save_raw
 from etl.transform import transform_data
-from etl.load import save_parquet, print_summary
+from etl.validate import validate_df
+from etl.load import save_parquet, load_to_db
 
 def main():
     parser = argparse.ArgumentParser(description="ETL pipeline")
     parser.add_argument("file_id", help="FILE_ID CSV на Google Drive")
     args = parser.parse_args()
 
-    #  Extract
+    # === Extract ===
     df = extract_data(args.file_id)
     save_raw(df)
 
-    #  Transform
+    # === Transform ===
     df = transform_data(df)
 
-    #  Load
+    # === Validate ===
+    validate_df(df)
+
+    # === Load ===
     save_parquet(df)
-    print_summary(df)
+
+    # Параметры подключения к базе данных (подставь свои)
+    db_config = {
+        "user": "ваш_user",
+        "password": "ваш_pass",
+        "url": "ваш_url",
+        "port": 5432,
+        "database": "homeworks"
+    }
+    load_to_db(df, db_config, max_rows=100)
 
 if __name__ == "__main__":
     main()
+
